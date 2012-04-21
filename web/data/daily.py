0378fd1f500450f8b91001d3bceceec7d9274b26
David Goodwin <david@zx.net.nz>
2012-04-21 21:14:37 +1200

    Split the data module up into a few submodules.

--- web/data/daily.py
+++ web/data/daily.py
@@ -1,30 +1,20 @@
 """
-Provides access to zxweather data over HTTP in a number of formats. Used for
-generating charts in JavaScript, etc.
+Provides access to zxweather daily data over HTTP in a number of formats.
+Used for generating charts in JavaScript, etc.
 """
 
-from datetime import date, datetime, time, timedelta
-from time import mktime
+from datetime import date, datetime, timedelta
 import web
 from config import db
 import config
 import json
-from wsgiref.handlers import format_date_time
+from data.util import rfcformat, datetime_to_js_date, outdoor_sample_result_to_datatable, pretty_print
 
 __author__ = 'David Goodwin'
-
-# TODO: round temperatures, etc.
-
-# Pretty-print JSON output? (makes debugging easier)
-pretty_print = False
 
 ### Data URLs:
 # This file provides URLs to access raw data in json format.
 #
-# /data/{year}/{month}/datatable/samples.json
-#       All samples for the month.
-# /data/{year}/{month}/datatable/30m_avg_samples.json
-#       30 minute averages for the month.
 # /data/{year}/{month}/{day}/datatable/samples.json
 #       All samples for the day.
 # /data/{year}/{month}/{day}/datatable/7day_samples.json
@@ -38,159 +28,9 @@ pretty_print = False
 # /data/{year}/{month}/{day}/datatable/7day_30m_avg_indoor_samples.json
 #       30 minute averages for the past 7 days of indoor weather.
 
+# TODO: round temperatures, etc.
 
-#############################
-## Helper Functions
-def datetime_to_js_date(timestamp):
-    """
-    Converts a python datetime.datetime or datetime.date object to a
-    representation compatbile with googles DataTable structure
-    :param timestamp: A timestamp
-    :return: datatable representation
-    """
-    if timestamp is None:
-        return "null"
-    elif isinstance(timestamp, datetime):
-        return "Date({0},{1},{2},{3},{4},{5})".format(timestamp.year,
-                                                      timestamp.month - 1,
-                                                      timestamp.day,
-                                                      timestamp.hour,
-                                                      timestamp.minute,
-                                                      timestamp.second)
-    elif isinstance(timestamp, date):
-        return "Date({0},{1},{2})".format(timestamp.year,
-                                          timestamp.month - 1, # for JS
-                                          timestamp.day)
-    elif isinstance(timestamp, time):
-        return [timestamp.hour, timestamp.minute, timestamp.second]
-    else:
-        return type(timestamp)
-        #raise TypeError
-
-def rfcformat(dt):
-    return format_date_time(mktime(dt.timetuple()))
-
-#############################
-## Monthly DataTable datasets
-class month_datatable_json:
-    """
-    Gets data for a particular month in Googles DataTable format.
-    """
-
-    def GET(self, station, year, month, dataset):
-        if station != config.default_station_name:
-            raise web.NotFound()
-
-        year = int(year)
-        month = int(month)
-
-        if dataset == 'samples':
-            return get_month_samples_datatable(year,month)
-        elif dataset == '30m_avg_samples':
-            return get_30m_avg_month_samples_datatable(year,month)
-        else:
-            raise web.NotFound()
-
-def monthly_cache_control_headers(year,month,age):
-    """
-    Sets cache control headers for a daily data files.
-    :param year: Year of the data file
-    :type year: int
-    :param month: Month of the data file
-    :type month: int
-    :param age: Timestamp of the last record in the data file
-    :type age: datetime
-    """
-
-    now = datetime.now()
-    # HTTP-1.1 Cache Control
-    if year == now.year and month == now.month:
-        # We should be getting a new sample every sample_interval seconds if
-        # the requested day is today.
-        web.header('Cache-Control', 'max-age=' + str(config.sample_interval))
-        web.header('Expires',
-                   rfcformat(age + timedelta(0, config.sample_interval)))
-    else:
-        # Old data. Never expires.
-        web.header('Expires',
-                   rfcformat(now + timedelta(60, 0))) # Age + 60 days
-    web.header('Last-Modified', rfcformat(age))
-
-def get_30m_avg_month_samples_datatable(year, month):
-
-    params = dict(date = date(year,month,01))
-    query_data = db.query("""select min(iq.time_stamp) as time_stamp,
-       avg(iq.temperature) as temperature,
-       avg(iq.dew_point) as dew_point,
-       avg(iq.apparent_temperature) as apparent_temperature,
-       avg(wind_chill) as wind_chill,
-       avg(relative_humidity)::integer as relative_humidity,
-       avg(absolute_pressure) as absolute_pressure,
-       min(prev_sample_time) as prev_sample_time,
-       bool_or(gap) as gap
-from (
-        select cur.time_stamp,
-               (extract(epoch from cur.time_stamp) / 1800)::integer AS quadrant,
-               cur.temperature,
-               cur.dew_point,
-               cur.apparent_temperature,
-               cur.wind_chill,
-               cur.relative_humidity,
-               cur.absolute_pressure,
-               cur.time_stamp - (cur.sample_interval * '1 minute'::interval) as prev_sample_time,
-               CASE WHEN (cur.time_stamp - prev.time_stamp) > ((cur.sample_interval * 2) * '1 minute'::interval) THEN
-                  true
-               else
-                  false
-               end as gap
-        from sample cur, sample prev
-        where date(date_trunc('month',cur.time_stamp)) = date(date_trunc('month',now()))
-          and prev.time_stamp = (select max(time_stamp) from sample where time_stamp < cur.time_stamp)
-        order by cur.time_stamp asc) as iq
-where date_trunc('month',iq.time_stamp) = date_trunc('month', now())
-group by iq.quadrant
-order by iq.quadrant asc""", params)
-
-    data, data_age = outdoor_sample_result_to_datatable(query_data)
-
-    monthly_cache_control_headers(year,month,data_age)
-
-    web.header('Content-Type','application/json')
-    web.header('Content-Length', str(len(data)))
-    return data
-
-def get_month_samples_datatable(year,month):
-
-    params = dict(date = date(year,month,01))
-    query_data = db.query("""select cur.time_stamp,
-       cur.temperature,
-       cur.dew_point,
-       cur.apparent_temperature,
-       cur.wind_chill,
-       cur.relative_humidity,
-       cur.absolute_pressure,
-       cur.time_stamp - (cur.sample_interval * '1 minute'::interval) as prev_sample_time,
-       CASE WHEN (cur.time_stamp - prev.time_stamp) > ((cur.sample_interval * 2) * '1 minute'::interval) THEN
-          true
-       else
-          false
-       end as gap
-from sample cur, sample prev
-where date(date_trunc('month',cur.time_stamp)) = $date
-  and prev.time_stamp = (select max(time_stamp) from sample where time_stamp < cur.time_stamp)
-order by cur.time_stamp asc""", params)
-
-    data, data_age = outdoor_sample_result_to_datatable(query_data)
-
-    monthly_cache_control_headers(year,month,data_age)
-
-    web.header('Content-Type','application/json')
-    web.header('Content-Length', str(len(data)))
-    return data
-
-#############################
-## Daily DataTable datasets
-class day_datatable_json:
+class datatable_json:
     """
     Gets data for a particular day in Googles DataTable format.
     """
@@ -307,80 +147,6 @@ def indoor_sample_result_to_datatable(query_data):
         return json.dumps(data, default=dthandler, sort_keys=True, indent=4), data_age
     else:
         return json.dumps(data, default=dthandler), data_age
-
-def outdoor_sample_result_to_datatable(query_data):
-    """
-    Converts a query on the sample table to DataTable-compatible JSON.
-
-    Expected columns are time_stamp, temperature, dew_point,
-    apparent_temperature, wind_chill, relative_humidity, absolute_pressure,
-    prev_sample_time, gap.
-
-    :param query_data: Result from the query
-    :return: Query data in JSON format.
-    """
-    cols = [{'id': 'timestamp',
-             'label': 'Time Stamp',
-             'type': 'datetime'},
-            {'id': 'temperature',
-             'label': 'Temperature',
-             'type': 'number'},
-            {'id': 'dewpoint',
-             'label': 'Dewpoint',
-             'type': 'number'},
-            {'id': 'apparenttemp',
-             'label': 'Apparent Temperature',
-             'type': 'number'},
-            {'id': 'windchill',
-             'label': 'Wind Chill',
-             'type': 'number'},
-            {'id': 'humidity',
-             'label': 'Humidity',
-             'type': 'number'},
-            {'id': 'abspressure',
-             'label': 'Absolute Pressure',
-             'type': 'number'},
-    ]
-
-    rows = []
-
-    # At the end of the following loop, this will contain the timestamp for
-    # the most recent record in this data set.
-    data_age = None
-
-    for record in query_data:
-
-        # Handle gaps in the dataset
-        if record.gap:
-            rows.append({'c': [{'v': datetime_to_js_date(record.prev_sample_time)},
-                    {'v': None},
-                    {'v': None},
-                    {'v': None},
-                    {'v': None},
-                    {'v': None},
-                    {'v': None},
-            ]
-            })
-
-        rows.append({'c': [{'v': datetime_to_js_date(record.time_stamp)},
-                {'v': record.temperature},
-                {'v': record.dew_point},
-                {'v': record.apparent_temperature},
-                {'v': record.wind_chill},
-                {'v': record.relative_humidity},
-                {'v': record.absolute_pressure},
-        ]
-        })
-
-        data_age = record.time_stamp
-
-    data = {'cols': cols,
-            'rows': rows}
-
-    if pretty_print:
-        return json.dumps(data, sort_keys=True, indent=4), data_age
-    else:
-        return json.dumps(data), data_age
 
 def get_7day_indoor_samples_datatable(year,month,day):
     """
@@ -552,38 +318,38 @@ where date(iq.time_stamp) > (date($date) - '7 days'::interval)
 group by iq.quadrant
 order by iq.quadrant asc""", params)
 
-# 1-hour average query:
-#    """select iq.time_stamp,
-#       avg(iq.temperature) as temperature,
-#       avg(iq.dew_point) as dew_point,
-#       avg(iq.apparent_temperature) as apparent_temperature,
-#       avg(wind_chill) as wind_chill,
-#       avg(relative_humidity)::integer as relative_humidity,
-#       avg(absolute_pressure) as absolute_pressure,
-#       min(prev_sample_time) as prev_sample_time,
-#       bool_or(gap) as gap
-#from (
-#        select date_trunc('hour',cur.time_stamp) as time_stamp,
-#               cur.temperature,
-#               cur.dew_point,
-#               cur.apparent_temperature,
-#               cur.wind_chill,
-#               cur.relative_humidity,
-#               cur.absolute_pressure,
-#               cur.time_stamp - (cur.sample_interval * '1 minute'::interval) as prev_sample_time,
-#               CASE WHEN (cur.time_stamp - prev.time_stamp) > ((cur.sample_interval * 2) * '1 minute'::interval) THEN
-#                  true
-#               else
-#                  false
-#               end as gap
-#        from sample cur, sample prev
-#        where date(date_trunc('month',cur.time_stamp)) = date(date_trunc('month',$date))
-#          and prev.time_stamp = (select max(time_stamp) from sample where time_stamp < cur.time_stamp)
-#        order by cur.time_stamp asc) as iq
-#where date(iq.time_stamp) >= (date($date) - '7 days'::interval)
-#group by iq.time_stamp
-#order by iq.time_stamp asc
-#    """
+    # 1-hour average query:
+    #    """select iq.time_stamp,
+    #       avg(iq.temperature) as temperature,
+    #       avg(iq.dew_point) as dew_point,
+    #       avg(iq.apparent_temperature) as apparent_temperature,
+    #       avg(wind_chill) as wind_chill,
+    #       avg(relative_humidity)::integer as relative_humidity,
+    #       avg(absolute_pressure) as absolute_pressure,
+    #       min(prev_sample_time) as prev_sample_time,
+    #       bool_or(gap) as gap
+    #from (
+    #        select date_trunc('hour',cur.time_stamp) as time_stamp,
+    #               cur.temperature,
+    #               cur.dew_point,
+    #               cur.apparent_temperature,
+    #               cur.wind_chill,
+    #               cur.relative_humidity,
+    #               cur.absolute_pressure,
+    #               cur.time_stamp - (cur.sample_interval * '1 minute'::interval) as prev_sample_time,
+    #               CASE WHEN (cur.time_stamp - prev.time_stamp) > ((cur.sample_interval * 2) * '1 minute'::interval) THEN
+    #                  true
+    #               else
+    #                  false
+    #               end as gap
+    #        from sample cur, sample prev
+    #        where date(date_trunc('month',cur.time_stamp)) = date(date_trunc('month',$date))
+    #          and prev.time_stamp = (select max(time_stamp) from sample where time_stamp < cur.time_stamp)
+    #        order by cur.time_stamp asc) as iq
+    #where date(iq.time_stamp) >= (date($date) - '7 days'::interval)
+    #group by iq.time_stamp
+    #order by iq.time_stamp asc
+    #    """
 
     data,age = outdoor_sample_result_to_datatable(result)
 
@@ -623,6 +389,4 @@ where date(s.time_stamp) = $date
     web.header('Content-Type','application/json')
     web.header('Content-Length', str(len(data)))
     return data
-
-
 
