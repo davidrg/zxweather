52c781f38968bad434784553e0f1772e1b5e35d0
david <david@localhost>
2012-05-13 18:28:46 +1200

    Move configuration to a file (#20)

--- web/config.py
+++ web/config.py
@@ -3,14 +3,11 @@ __author__ = 'David Goodwin'
 import web
 
 # Database configuration
-db = web.database(dbn='postgres',
-                  user='zxweather',
-                  pw='password',
-                  db='weather')
+db = None
 
 # This is the name of the default station. In a future version
 # this will live in the database instead.
-default_station_name = 'rua' # Ruakura, Hamilton
+default_station_name = None
 
 # Set this to True if you are running wh1080d to feed samples and live data
 # into the database.
@@ -23,10 +20,61 @@ sample_interval = 300 # 5 minutes
 plot_interval = 1800
 
 # Where static data lives
-static_data_dir = 'Z:/current/zxweather/web/static/'
+static_data_dir = None
 
 # Root directory of the site. Required when doing 301/302 redirects.
-site_root = "http://localhost:8080/"
+site_root = None
 
 # Set to None to use the UI picker page.
 default_ui = 's'
+
+def load_settings():
+    """
+    Loads settings from the configuration file.
+    """
+
+    global db, default_station_name, live_data_available, sample_interval
+    global plot_interval, static_data_dir, site_root, default_ui
+
+    import ConfigParser
+    config = ConfigParser.ConfigParser()
+    config.read('Z:/current/zxweather/web/config.cfg')
+
+    # Configuration sections
+    S_DB = 'database'   # Database configuration
+    S_D = 'data'        # Data configuration
+    S_S = 'site'        # Site configuration
+
+    # Make sure a few important settings people might overlook are set.
+    if not config.has_option(S_S,'site_root'):
+        raise Exception("ConfigurationError: site_root not specified. Consult installation reference manual.")
+    if not config.has_option(S_S,'static_data_dir'):
+        raise Exception("ConfigurationError: static data location not specified. Consult installation reference manual.")
+    if not config.has_option(S_S,'station_name'):
+        raise Exception("ConfigurationError: station name not specified. Consult installation reference manual.")
+
+    # Database
+    hostname = config.get(S_DB,'host')
+    port = config.getint(S_DB,'port')
+    database = config.get(S_DB,'database')
+    user = config.get(S_DB,'user')
+    pw = config.get(S_DB,'password')
+    db = web.database(dbn='postgres',
+                      user=user,
+                      pw=pw,
+                      db=database,
+                      host=hostname,
+                      port=port)
+
+    # Data
+    live_data_available = config.getboolean(S_D,'live_data_available')
+    sample_interval = config.getint(S_D,'sample_interval')
+    plot_interval = config.getint(S_D,'plot_interval')
+
+    # Site
+    default_station_name = config.get(S_S, 'station_name')
+    static_data_dir = config.get(S_S, 'static_data_dir')
+    site_root = config.get(S_S, 'site_root')
+    default_ui = config.get(S_S, 'default_ui')
+
+
