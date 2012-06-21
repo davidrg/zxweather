e0f19fcb69776f35a3021004def952d6063c26f6
David Goodwin <david@zx.net.nz>
2012-06-21 22:27:25 +1200

    Reduce duplicate code

--- zxw_web/static/js/day_charts.js
+++ zxw_web/static/js/day_charts.js
@@ -5,58 +5,6 @@
  */
 
 
-function drawAllCharts(data,
-                       tdp_element,
-                       awc_element,
-                       humidity_element,
-                       pressure_element) {
-
-    // Temperature and Dewpoint only
-    var temperature_tdp = new google.visualization.DataView(data);
-    temperature_tdp.hideColumns([3,4,5,6]);
-
-    var temperature_tdp_options = {
-        title: 'Temperature and Dew Point (°C)',
-        legend: {position: 'bottom'}
-    };
-    var temperature_tdp_chart = new google.visualization.LineChart(tdp_element);
-    temperature_tdp_chart.draw(temperature_tdp, temperature_tdp_options);
-
-    // Apparent Temperature and Wind Chill only
-    var temperature_awc = new google.visualization.DataView(data);
-    temperature_awc.hideColumns([1,2,5,6]);
-
-    var temperature_awc_options = {
-        title: 'Apparent Temperature and Wind Chill (°C)',
-        legend: {position: 'bottom'}
-    };
-    var temperature_awc_chart = new google.visualization.LineChart(awc_element);
-    temperature_awc_chart.draw(temperature_awc, temperature_awc_options);
-
-    // Absolute Pressure only
-    var pressure = new google.visualization.DataView(data);
-    pressure.hideColumns([1,2,3,4,5]);
-
-    var pressure_options = {
-        title: 'Absolute Pressure (hPa)',
-        legend: {position: 'none'},
-        vAxis: {format: '####'}
-    };
-    var pressure_chart = new google.visualization.LineChart(pressure_element);
-    pressure_chart.draw(pressure, pressure_options);
-
-    // Humidity only
-    var humidity = new google.visualization.DataView(data);
-    humidity.hideColumns([1,2,3,4,6]);
-
-    var humidity_options = {
-        title: 'Humidity (%)',
-        legend: {position: 'none'}
-    };
-    var humidity_chart = new google.visualization.LineChart(humidity_element);
-    humidity_chart.draw(humidity, humidity_options);
-}
-
 // Loads all daily charts.
 function load_day_charts() {
     $("#day_charts_cont").show();
@@ -93,11 +41,11 @@ function load_day_charts() {
             { pattern: '####.# hPa'});
         pressureFormatter.format(sampledata,6);
 
-        drawAllCharts(sampledata,
-                      document.getElementById('chart_temperature_tdp_div'),
-                      document.getElementById('chart_temperature_awc_div'),
-                      document.getElementById('chart_humidity_div'),
-                      document.getElementById('chart_pressure_div')
+        drawAllLineCharts(sampledata,
+                          document.getElementById('chart_temperature_tdp_div'),
+                          document.getElementById('chart_temperature_awc_div'),
+                          document.getElementById('chart_humidity_div'),
+                          document.getElementById('chart_pressure_div')
         );
     }).error(function() {
                  $("#day_charts_cont").hide();
@@ -207,11 +155,11 @@ function load_7day_charts() {
             { pattern: '####.# hPa'});
         pressureFormatter.format(sampledata,6);
 
-        drawAllCharts(sampledata,
-                      document.getElementById('chart_7_temperature_tdp_div'),
-                      document.getElementById('chart_7_temperature_awc_div'),
-                      document.getElementById('chart_7_humidity_div'),
-                      document.getElementById('chart_7_pressure_div')
+        drawAllLineCharts(sampledata,
+                          document.getElementById('chart_7_temperature_tdp_div'),
+                          document.getElementById('chart_7_temperature_awc_div'),
+                          document.getElementById('chart_7_humidity_div'),
+                          document.getElementById('chart_7_pressure_div')
         );
     }).error(function() {
                  $("#7day-charts").hide();
