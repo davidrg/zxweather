03725579a46599e9e2bafcc7e80b8e3985cfd835
david <david@localhost>
2012-06-23 01:10:15 +1200

    Basic code to fetch live data and display it.

---
+++ desktop/database.h
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ *            Created: 23/06/2012
+ *          Copyright: (C) Copyright David Goodwin, 2012
+ *            License: GNU General Public License
+ *****************************************************************************
+ *
+ *   This is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This software is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this software; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ ****************************************************************************/
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+typedef struct _live_data_record {
+    int relative_humidity;
+    float temperature;
+    float dew_point;
+    float wind_chill;
+    float apparent_temperature;
+    float absolute_pressure;
+    float average_wind_speed;
+    float gust_wind_speed;
+    char wind_direction[4];
+} live_data_record;
+
+void wdb_connect(const char *target, const char *username, const char* password);
+
+live_data_record wdb_get_live_data();
+
+#endif // DATABASE_H
---
+++ desktop/desktop.pro
@@ -0,0 +1,38 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-06-23T00:07:02
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = desktop
+TEMPLATE = app
+
+# For building pgc sources with ECPG on Windows
+ecpg.name = Process Embedded SQL sources
+ecpg.input = ECPG_SOURCES
+ecpg.output = ${QMAKE_FILE_BASE}.cpp
+ecpg.commands = ../desktop/tools/ecpg-9.1-win32/ecpg.exe -o ${QMAKE_FILE_OUT} ${QMAKE_FILE_IN}
+ecpg.variable_out = SOURCES
+ecpg.dependency_type = TYPE_CXX
+QMAKE_EXTRA_COMPILERS += ecpg
+
+win32:LIBS += -L../desktop/lib/libecpg-9.1-win32 -lecpg
+win32:INCLUDEPATH += lib/libecpg-9.1-win32/include
+
+ECPG_SOURCES += database.pgc
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+    database.h
+
+FORMS    += mainwindow.ui
+
+OTHER_FILES += \
+    database.pgc
+
