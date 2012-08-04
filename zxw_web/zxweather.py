cfe520751d469c292adba9a508c243c00d84b619
David Goodwin <david@zx.net.nz>
2012-08-04 18:19:47 +1200

    Big refactoring of daily JSON data sources. A significant amount of duplicate code has now been removed.

--- zxw_web/zxweather.py
+++ zxw_web/zxweather.py
@@ -25,15 +25,15 @@ urls = (
     # Data sources (mostly JSON)
     # /data/station/year/month/day/... (month is in number format here)
     # All code resides in the data package.
-    '/data/(\w*)/(\d+)/(\d+)/(\d+)/datatable/(\w*).json', 'data.daily_datatable.dt_json', # Daily, DT
-        '/data/(\w*)/(\d+)/(\d+)/(\d+)/(\w*).json', 'data.daily.data_json',            # Daily
-    '/data/(\w*)/(\d+)/(\d+)/datatable/(\w*).json', 'data.monthly.datatable_json',     # Monthly, DT
-    '/data/(\w*)/(\d+)/(\d+)/(\w*).json', 'data.monthly.data_json',                    # Monthly,
-    '/data/(\w*)/(\d+)/datatable/(\w*).json', 'data.yearly.datatable_json',            # Yearly, DT
-    '/data/(\w*)/(\d+)/(\w*).json', 'data.yearly.data_json',                           # Yearly
-    '/data/(\w*)/datatable/(\w*).json', 'data.station.datatable_json',                 # Station, DT
-    '/data/(\w*)/(\w*).json', 'data.station.data_json',                                # Station
-    '/data/daynav.json', 'data.day_nav',                                               # Global
+    '/data/(\w*)/(\d+)/(\d+)/(\d+)/datatable/(\w*).json', 'data.daily.dt_json',     # Daily, DT
+        '/data/(\w*)/(\d+)/(\d+)/(\d+)/(\w*).json', 'data.daily.data_json',         # Daily
+    '/data/(\w*)/(\d+)/(\d+)/datatable/(\w*).json', 'data.monthly.datatable_json',  # Monthly, DT
+    '/data/(\w*)/(\d+)/(\d+)/(\w*).json', 'data.monthly.data_json',                 # Monthly,
+    '/data/(\w*)/(\d+)/datatable/(\w*).json', 'data.yearly.datatable_json',         # Yearly, DT
+    '/data/(\w*)/(\d+)/(\w*).json', 'data.yearly.data_json',                        # Yearly
+    '/data/(\w*)/datatable/(\w*).json', 'data.station.datatable_json',              # Station, DT
+    '/data/(\w*)/(\w*).json', 'data.station.data_json',                             # Station
+    '/data/daynav.json', 'data.day_nav',                                            # Global
 
     # User interface (HTML output).
     # /ui/station/year/month/day
