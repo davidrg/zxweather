c16f17607cafd9d2e411bf4902102721b3b18653
David Goodwin <david@zx.net.nz>
2013-02-07 22:10:17 +1300

    An initial TCP data source (for the zxweather server)

--- desktop/datasource/abstractlivedatasource.h
+++ desktop/datasource/abstractlivedatasource.h
@@ -25,6 +25,10 @@ typedef struct _liveData {
     bool indoorDataAvailable;
 } LiveDataSet;
 
+/** This is an interface for all data sources that can provide live data.
+ * Data sources that can also provide samples should inherit from
+ * AbstractDataSource instead.
+ */
 class AbstractLiveDataSource : public QObject
 {
     Q_OBJECT
@@ -32,9 +36,21 @@ class AbstractLiveDataSource : public QObject
 public:
     explicit AbstractLiveDataSource(QObject* parent=0): QObject(parent) {}
 
+    /** Enables the live data feed. The data source will not supply any live
+     * data until this is called.
+     */
     virtual void enableLiveData() = 0;
 signals:
+    /** Emitted when ever live data is available.
+     *
+     * @param data The live data.
+     */
     void liveData(LiveDataSet data);
+
+    /** Emitted when ever an error occurs.
+     *
+     * @param errMsg The error message.
+     */
     void error(QString errMsg);
 };
 
