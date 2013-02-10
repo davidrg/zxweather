60bf980bb1f5d090d0b2b2950d8c911084b2779b
David Goodwin <david@zx.net.nz>
2013-02-10 21:25:18 +1300

    handle 'dashed' values in LOOP records. The documentation doesn't specify what the dashed values are so I'm working off the assumption that they're the same as for DMP records. The data certainly looks as if that is the case.

--- davis-logger/davis_logger/record_types/dmp.py
+++ davis-logger/davis_logger/record_types/dmp.py
@@ -5,8 +5,10 @@ The format used by the DMP and DMPAFT commands.
 from collections import namedtuple
 import struct
 import datetime
-from davis_logger.record_types.util import f_to_c, inhg_to_mb, mph_to_ms, \
-    inch_to_mm, c_to_f, mb_to_inhg, ms_to_mph, mm_to_inch, CRC
+from davis_logger.record_types.util import inhg_to_mb, mph_to_ms, \
+    inch_to_mm, mb_to_inhg, ms_to_mph, mm_to_inch, CRC, deserialise_8bit_temp, \
+    serialise_8bit_temp, undash_8bit, dash_8bit, deserialise_16bit_temp, \
+    serialise_16bit_temp
 
 __author__ = 'david'
 
@@ -161,73 +163,6 @@ def encode_time(record_time):
     return record_time.hour * 100 + record_time.minute
 
 
-def _deserialise_8bit_temp(temp):
-    """
-    Converts an 8-bit temperature value to degrees C. If its dashed it converts
-    it to None instead.
-    :param temp: temp in deg F minus 90
-    :rtype: int
-    """
-    if temp == 255:
-        return None
-    else:
-        return f_to_c(temp + 90)
-
-
-def _serialise_8bit_temp(temp):
-    """
-    Converts the supplied temperature to the format used in DMP records
-    :param temp: Temperature to convert
-    :return: Serialised temperature
-    :rtype: int
-    """
-
-    if temp is None:
-        return 255
-
-    return int(c_to_f(temp) - 90)
-
-
-def _deserialise_16bit_temp(temp, minDashed=False):
-    """
-    Converts the 16bit temperature value to degrees C. If its dashed it
-    converts it to None instead.
-    :param temp: Temperature to convert
-    :type temp: int
-    :param minDashed: If the minimum dashed value (-32768) should be used
-    instead
-    :type minDashed: bool
-    :return: Temperature in degrees C
-    :rtype: float or None
-    """
-
-    if temp == 32767 and not minDashed:
-        return None
-    elif temp == -32767 and minDashed:
-        return None
-    else:
-        return f_to_c(temp / 10.0)
-
-
-def _serialise_16bit_temp(temp, minDashed=False):
-    """
-    Converts the supplied 16bit temperature to the format used by DMP records.
-    :param temp: The temperature to convert
-    :type temp: float
-    :param minDashed: If the minimum dashed value should be used
-    :type minDashed: bool
-    :return: temp value
-    :rtype: int
-    """
-
-    if temp is None and minDashed:
-        return -32767
-    elif temp is None:
-        return 32767
-
-    return int(c_to_f(temp) * 10.0)
-
-
 _compass_points = [
     0,  # N
     22.5,  # NNE
@@ -276,28 +211,6 @@ def _serialise_wind_direction_code(value):
     return _compass_points.index(value)
 
 
-def _undash_8bit(value):
-    """
-    Returns None if the value is 255, otherwise the value is returned.
-    :param value: Value to check
-    """
-    if value == 255:
-        return None
-    else:
-        return value
-
-
-def _dash_8bit(value):
-    """
-    Returns 255 if the supplied value is None, otherwise the value is returned.
-    :param value: Value to check
-    """
-    if value is None:
-        return 255
-    else:
-        return value
-
-
 def deserialise_dmp(dmp_string):
     """
     Deserialised a dmp string into a Dmp tuple.
@@ -340,18 +253,18 @@ def deserialise_dmp(dmp_string):
     unpacked = Dmp(
         dateStamp=decode_date(dateStamp),
         timeStamp=decode_time(timeStamp),
-        outsideTemperature=_deserialise_16bit_temp(outsideTemperature),
-        highOutsideTemperature=_deserialise_16bit_temp(
+        outsideTemperature=deserialise_16bit_temp(outsideTemperature),
+        highOutsideTemperature=deserialise_16bit_temp(
             highOutsideTemperature, True),
-        lowOutsideTemperature=_deserialise_16bit_temp(lowOutsideTemperature),
+        lowOutsideTemperature=deserialise_16bit_temp(lowOutsideTemperature),
         rainfall=rainfall,  # TODO: This depends on station config
         highRainRate=highRainRate,  # TODO: this depends on station config
         barometer=inhg_to_mb(barometer / 1000.0),
         solarRadiation=solarRadiation,
         numberOfWindSamples=numberOfWindSamples,
-        insideTemperature=_deserialise_16bit_temp(insideTemperature),
-        insideHumidity=_undash_8bit(insideHumidity),
-        outsideHumidity=_undash_8bit(outsideHumidity),
+        insideTemperature=deserialise_16bit_temp(insideTemperature),
+        insideHumidity=undash_8bit(insideHumidity),
+        outsideHumidity=undash_8bit(outsideHumidity),
         averageWindSpeed=averageWindSpeed,
         highWindSpeed=mph_to_ms(highWindSpeed),
         highWindSpeedDirection=_deserialise_wind_direction_code(
@@ -364,33 +277,33 @@ def deserialise_dmp(dmp_string):
         highUVIndex=highUVIndex,
         forecastRule=forecastRule,
         leafTemperature=[
-            _deserialise_8bit_temp(leafTemperature_1),
-            _deserialise_8bit_temp(leafTemperature_2)
+            deserialise_8bit_temp(leafTemperature_1),
+            deserialise_8bit_temp(leafTemperature_2)
         ],
         leafWetness=[
-            _undash_8bit(leafWetness_1),
-            _undash_8bit(leafWetness_2)
+            undash_8bit(leafWetness_1),
+            undash_8bit(leafWetness_2)
         ],
         soilTemperatures=[
-            _deserialise_8bit_temp(soilTemperature_1),
-            _deserialise_8bit_temp(soilTemperature_2),
-            _deserialise_8bit_temp(soilTemperature_3),
-            _deserialise_8bit_temp(soilTemperature_4)
+            deserialise_8bit_temp(soilTemperature_1),
+            deserialise_8bit_temp(soilTemperature_2),
+            deserialise_8bit_temp(soilTemperature_3),
+            deserialise_8bit_temp(soilTemperature_4)
         ],
         extraHumidities=[
-            _undash_8bit(extraHumidity_1),
-            _undash_8bit(extraHumidity_2)
+            undash_8bit(extraHumidity_1),
+            undash_8bit(extraHumidity_2)
         ],
         extraTemperatures=[
-            _deserialise_8bit_temp(extraTemperature_1),
-            _deserialise_8bit_temp(extraTemperature_2),
-            _deserialise_8bit_temp(extraTemperature_3)
+            deserialise_8bit_temp(extraTemperature_1),
+            deserialise_8bit_temp(extraTemperature_2),
+            deserialise_8bit_temp(extraTemperature_3)
         ],
         soilMoistures=[
-            _undash_8bit(soilMoisture_1),
-            _undash_8bit(soilMoisture_2),
-            _undash_8bit(soilMoisture_3),
-            _undash_8bit(soilMoisture_4)
+            undash_8bit(soilMoisture_1),
+            undash_8bit(soilMoisture_2),
+            undash_8bit(soilMoisture_3),
+            undash_8bit(soilMoisture_4)
         ]
     )
 
@@ -428,17 +341,17 @@ def serialise_dmp(dmp):
         dmp_format,
         encode_date(dmp.dateStamp),
         encode_time(dmp.timeStamp),
-        _serialise_16bit_temp(dmp.outsideTemperature),
-        _serialise_16bit_temp(dmp.highOutsideTemperature, True),
-        _serialise_16bit_temp(dmp.lowOutsideTemperature),
+        serialise_16bit_temp(dmp.outsideTemperature),
+        serialise_16bit_temp(dmp.highOutsideTemperature, True),
+        serialise_16bit_temp(dmp.lowOutsideTemperature),
         dmp.rainfall,  # TODO: This depends on station config
         dmp.highRainRate,  # TODO: This depends on station config
         mb_to_inhg(dmp.barometer * 1000),
         solarRadiation,
         dmp.numberOfWindSamples,
-        _serialise_16bit_temp(dmp.insideTemperature),
-        _dash_8bit(dmp.insideHumidity),
-        _dash_8bit(dmp.outsideHumidity),
+        serialise_16bit_temp(dmp.insideTemperature),
+        dash_8bit(dmp.insideHumidity),
+        dash_8bit(dmp.outsideHumidity),
         averageWindSpeed,
         ms_to_mph(dmp.highWindSpeed),
         _serialise_wind_direction_code(dmp.highWindSpeedDirection),
@@ -448,24 +361,24 @@ def serialise_dmp(dmp):
         dmp.highSolarRadiation,
         dmp.highUVIndex,
         dmp.forecastRule,
-        _serialise_8bit_temp(dmp.leafTemperature[0]),
-        _serialise_8bit_temp(dmp.leafTemperature[1]),
-        _dash_8bit(dmp.leafWetness[0]),
-        _dash_8bit(dmp.leafWetness[1]),
-        _serialise_8bit_temp(dmp.soilTemperatures[0]),
-        _serialise_8bit_temp(dmp.soilTemperatures[1]),
-        _serialise_8bit_temp(dmp.soilTemperatures[2]),
-        _serialise_8bit_temp(dmp.soilTemperatures[3]),
+        serialise_8bit_temp(dmp.leafTemperature[0]),
+        serialise_8bit_temp(dmp.leafTemperature[1]),
+        dash_8bit(dmp.leafWetness[0]),
+        dash_8bit(dmp.leafWetness[1]),
+        serialise_8bit_temp(dmp.soilTemperatures[0]),
+        serialise_8bit_temp(dmp.soilTemperatures[1]),
+        serialise_8bit_temp(dmp.soilTemperatures[2]),
+        serialise_8bit_temp(dmp.soilTemperatures[3]),
         0,  # Record type. 0x00 == B, 0xFF == A
-        _dash_8bit(dmp.extraHumidities[0]),
-        _dash_8bit(dmp.extraHumidities[1]),
-        _serialise_8bit_temp(dmp.extraTemperatures[0]),
-        _serialise_8bit_temp(dmp.extraTemperatures[1]),
-        _serialise_8bit_temp(dmp.extraTemperatures[2]),
-        _dash_8bit(dmp.soilMoistures[0]),
-        _dash_8bit(dmp.soilMoistures[1]),
-        _dash_8bit(dmp.soilMoistures[2]),
-        _dash_8bit(dmp.soilMoistures[3])
+        dash_8bit(dmp.extraHumidities[0]),
+        dash_8bit(dmp.extraHumidities[1]),
+        serialise_8bit_temp(dmp.extraTemperatures[0]),
+        serialise_8bit_temp(dmp.extraTemperatures[1]),
+        serialise_8bit_temp(dmp.extraTemperatures[2]),
+        dash_8bit(dmp.soilMoistures[0]),
+        dash_8bit(dmp.soilMoistures[1]),
+        dash_8bit(dmp.soilMoistures[2]),
+        dash_8bit(dmp.soilMoistures[3])
     )
 
     return packed
