45e9b30f8977a08e7113e7dfdeb3e66cd0119d30
David Goodwin <david@zx.net.nz>
2013-02-16 20:13:05 +1300

    Initial support for pushing davis data. It all seems to work fine but sample push performance (in general) is very poor for large jobs at the moment as the server only inserts one sample at a time.

--- weather_push/zxw_push/database.py
+++ weather_push/zxw_push/database.py
@@ -27,7 +27,7 @@ class WeatherDatabase(object):
         self._upload_client = upload_client
         self._latest_ts = {}
 
-    def fetch_live(self, station_code):
+    def _fetch_generic_live(self, station_code):
         """
         Fetches live data and sends it off to the UploadClient for uploading.
         :param station_code: Station to get live data for.
@@ -56,10 +56,68 @@ class WeatherDatabase(object):
 
         def process_result(result, upload_client):
             station_data = result[0]
-            upload_client.sendLive(station_data)
+            upload_client.sendLive(station_data, 'GENERIC')
 
         self._conn.runQuery(query, (station_code,)).addCallback(
             process_result, self._upload_client)
+
+    def _fetch_davis_live(self, station_code):
+        """
+        Fetches live data from Davis weather stations and sends it off to the
+        UploadClient for uploading.
+        :param station_code: Station to get live data for.
+        """
+
+        # Don't bother sending live data for stations the remote system doesn't
+        # know anything about.
+        if station_code not in self._upload_client.latestSampleInfo:
+            return
+
+        query = """
+        select s.code as station_code,
+           ld.download_timestamp,
+           ld.indoor_relative_humidity as indoor_humidity,
+           ld.indoor_temperature,
+           ld.temperature,
+           ld.relative_humidity as humidity,
+           ld.absolute_pressure as pressure,
+           ld.average_wind_speed,
+           ld.gust_wind_speed,
+           ld.wind_direction,
+           dd.bar_trend,
+           dd.rain_rate,
+           dd.storm_rain,
+           dd.current_storm_start_date,
+           dd.transmitter_battery,
+           dd.console_battery_voltage,
+           dd.forecast_icon,
+           dd.forecast_rule_id
+    from live_data ld
+    inner join station s on s.station_id = ld.station_id
+    inner join davis_live_data dd on dd.station_id = ld.station_id
+    where s.code = %s
+        """
+
+        def process_result(result, upload_client):
+            station_data = result[0]
+            upload_client.sendLive(station_data, 'DAVIS')
+
+        self._conn.runQuery(query, (station_code,)).addCallback(
+            process_result, self._upload_client)
+
+    def _fetch_live(self, station_code):
+        """
+        Fetches live data for the specified weather station and sends it off
+        to the UploadClient for uploading.
+        :param station_code: Station to get live data for.
+        """
+
+        hw_type = self.station_code_hardware_type[station_code]
+
+        if hw_type == 'DAVIS':  # Davis hardware has extra data to send.
+            self._fetch_davis_live(station_code)
+        else:  # FineOffset and Generic hardware don't send any extra data
+            self._fetch_generic_live(station_code)
 
     def _get_latest_ts(self, station_code):
         if station_code not in self._latest_ts:
@@ -73,8 +131,12 @@ class WeatherDatabase(object):
         :param hw_type: Hardware type for the station.
         """
         for data in result:
-            self._upload_client.sendSample(data, hw_type)
+            self._upload_client.sendSample(data, hw_type, True)
             self._latest_ts[data['station_code']] = data['time_stamp']
+
+        self._upload_client.flushSamples()
+
+    # TODO: merge the three samples queries.
 
     def fetch_wh1080_samples(self, station_code, latest_ts):
         query = """
@@ -106,10 +168,20 @@ from sample s
 inner join station st on st.station_id = s.station_id
 inner join wh1080_sample wh on wh.sample_id = s.sample_id
 where st.code = %s
-  and s.time_stamp > %s
+        """
+        params = (station_code,)
+
+        # If there is no latest timestamp then the remote database is probably
+        # empty so we'll send everything.
+        if latest_ts is not None:
+            query += "  and s.time_stamp > %s"
+            params = (station_code, latest_ts)
+
+        query += """
+order by s.time_stamp asc
         """
 
-        self._conn.runQuery(query, (station_code, latest_ts,)).addCallback(
+        self._conn.runQuery(query, params).addCallback(
             self.process_samples_result, 'FOWH1080')
 
     def fetch_generic_samples(self, station_code, latest_ts):
@@ -128,12 +200,70 @@ select st.code as station_code,
        coalesce(s.time_stamp::varchar, 'None') as time_stamp
 from sample s
 inner join station st on st.station_id = s.station_id
-where st.code = %s
-  and s.time_stamp > %s
+where st.code = %
+        """
+        params = (station_code,)
+
+        # If there is no latest timestamp then the remote database is probably
+        # empty so we'll send everything.
+        if latest_ts is not None:
+            query += "  and s.time_stamp > %s"
+            params = (station_code, latest_ts)
+
+        query += """
+order by s.time_stamp asc
         """
 
-        self._conn.runQuery(query, (station_code, latest_ts,)).addCallback(
+        self._conn.runQuery(query, params).addCallback(
             self.process_samples_result, 'GENERIC')
+
+    def _fetch_davis_samples(self, station_code, latest_ts):
+        query = """
+select st.code as station_code,
+       coalesce(s.indoor_relative_humidity::varchar, 'None') as indoor_humidity,
+       coalesce(s.indoor_temperature::varchar, 'None') as indoor_temperature,
+       coalesce(s.temperature::varchar, 'None') as temperature,
+       coalesce(s.relative_humidity::varchar, 'None') as humidity,
+       coalesce(s.absolute_pressure::varchar, 'None') as pressure,
+       coalesce(s.average_wind_speed::varchar, 'None') as average_wind_speed,
+       coalesce(s.gust_wind_speed::varchar, 'None') as gust_wind_speed,
+       coalesce(s.wind_direction::varchar, 'None') as wind_direction,
+       coalesce(s.rainfall::varchar, 'None') as rainfall,
+       coalesce(s.download_timestamp::varchar, 'None') as download_timestamp,
+       coalesce(s.time_stamp::varchar, 'None') as time_stamp,
+
+       ds.record_time::varchar as record_time,
+       ds.record_date::varchar as record_date,
+       coalesce(ds.high_temperature::varchar, 'None') as high_temperature,
+       coalesce(ds.low_temperature::varchar, 'None') as low_temperature,
+       coalesce(ds.high_rain_rate::varchar, 'None') as high_rain_rate,
+       coalesce(ds.solar_radiation::varchar, 'None') as solar_radiation,
+       coalesce(ds.wind_sample_count::varchar, 'None') as wind_sample_count,
+       coalesce(ds.gust_wind_direction::varchar, 'None') as gust_wind_direction,
+       coalesce(ds.average_uv_index::varchar, 'None') as average_uv_index,
+       coalesce(ds.evapotranspiration::varchar, 'None') as evapotranspiration,
+       coalesce(ds.high_solar_radiation::varchar, 'None') as high_solar_radiation,
+       coalesce(ds.high_uv_index::varchar, 'None') as high_uv_index,
+       coalesce(ds.forecast_rule_id::varchar, 'None') as forecast_rule_id
+from sample s
+inner join station st on st.station_id = s.station_id
+inner join davis_sample ds on ds.sample_id = s.sample_id
+where st.code = %s
+        """
+        params = (station_code,)
+
+        # If there is no latest timestamp then the remote database is probably
+        # empty so we'll send everything.
+        if latest_ts is not None:
+            query += "  and s.time_stamp > %s"
+            params = (station_code, latest_ts)
+
+        query += """
+order by s.time_stamp asc
+        """
+
+        self._conn.runQuery(query, params).addCallback(
+            self.process_samples_result, 'DAVIS')
 
     def _fetch_samples(self, station_code):
         # Don't bother sending samples for stations the remote system doesn't
@@ -147,8 +277,11 @@ where st.code = %s
 
         if hw_type == 'FOWH1080':
             self.fetch_wh1080_samples(station_code, latest_ts)
-        elif hw_type == 'GENERIC':
+        elif hw_type == 'DAVIS':
+            self._fetch_davis_samples(station_code, latest_ts)
+        else:  # Its GENERIC or something unsupported.
             self.fetch_generic_samples(station_code, latest_ts)
+
 
     def observer(self, notify):
         """
@@ -162,7 +295,7 @@ where st.code = %s
             notify.channel, notify.payload)
 
         if notify.channel == "live_data_updated":
-            self.fetch_live(notify.payload)
+            self._fetch_live(notify.payload)
         elif notify.channel == "new_sample":
             self._fetch_samples(notify.payload)
 
