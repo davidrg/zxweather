7ba1bcae68423d309ced0f92a77dca2d799ca5f4
David Goodwin <david@zx.net.nz>
2013-07-18 23:14:02 +1200

    Caching! its all there and seems to be working properly. Needs more testing to ensure it properly handles gaps, etc.

--- desktop/constants.h
+++ desktop/constants.h
@@ -5,7 +5,7 @@
 
 namespace Constants {
 
-    const QByteArray USER_AGENT = "zxweather-desktop v1.0";
+    const QByteArray USER_AGENT = "zxweather-desktop/1.0";
     const QByteArray VERSION_STR = "1.0.0";
 
 }
--- desktop/datasource/webdatasource.h
+++ desktop/datasource/webdatasource.h
@@ -14,12 +14,20 @@
 
 typedef struct _data_file_t {
     QString filename;
-    uint32_t size;
+    int size;
     QDateTime last_modified;
-    QString name;
+    SampleSet samples;
+    bool isValid;
+    bool expireExisting;
 } data_file_t;
 
-class SampleDownloader;
+typedef struct _cache_stats_t {
+    int count;
+    QDateTime start;
+    QDateTime end;
+    bool isValid;
+} cache_stats_t;
+
 
 class WebDataSource : public AbstractDataSource
 {
@@ -34,43 +42,72 @@ public:
     void enableLiveData();
 
     hardware_type_t getHardwareType();
-
 private slots:
-    void dataReady(QNetworkReply* reply);
     void liveDataReady(QNetworkReply* reply);
     void liveDataPoll();
 
+    /**** Sample data network response handlers ****/
+    void requestFinished(QNetworkReply* reply);
+
 private:
+
+    /**** Sample data private functions ****/
+    QUrl buildRangeRequestURL() const;
+
+    void rangeRequestFinished(QNetworkReply* reply);
+
+
+    void makeNextCacheStatusRequest();
+    void cacheStatusRequestFinished(QNetworkReply* reply);
+
+    void makeNextDataRequest();
+    void downloadRequestFinished(QNetworkReply* reply);
+    void completeDataRequest();
+    data_file_t loadDataFile(QString url, QStringList fileData,
+                             QDateTime lastModified, int fileSize,
+                             cache_stats_t cacheStats);
+
+    SampleSet selectRequestedData();
+
+    typedef enum {
+        DLS_READY,          /*!< Not doing anything. Ready to fetch data */
+        DLS_INIT,           /*!< Setup */
+        DLS_RANGE_REQUEST,  /*!< Checking that the request range falls within
+                                 what the server has available */
+        DLS_CHECK_CACHE,    /*!< Checking which URLs are almost certainly
+                                 cached */
+        DLS_DOWNLOAD_DATA   /*!< We're downloading full data files */
+    } SampleDownloadState;
+
+    void setDownloadState(SampleDownloadState state) {download_state = state;}
+
+    void makeProgress(QString message);
+
+    void dlReset();
+
+    /**** Sample data member variables ****/
+    SampleDownloadState download_state;
+
+    // Time range we are currently downloading:
+    QDateTime dlStartTime, dlEndTime;
+
+    QStringList candidateURLs;
+    QStringList acceptedURLs;
+    QHash<QString, QString> urlNames;
+
     QString baseURL;
+    QString stationUrl; // Used as a key in the cache DB.
     QString stationCode;
 
-    QStringList urlQueue;
-    QStringList dataSetQueue;
-
-    QList<data_file_t> dataFileQueue;
-    data_file_t currentDataFile;
-
-    QStringList allData;
-    QDateTime start;
-    QDateTime end;
-
-    QDateTime minTimestamp;
-    QDateTime maxTimestamp;
 
     QScopedPointer<QNetworkAccessManager> netAccessManager;
 
-    QStringList failedDataSets;
-
-    void prepareNextDataSet();
-    void processData();
-    void rangeRequestResult(QString data);
-
+    // Local cache database functionality
     void openCache();
     bool isCached(QString station, QDate date);
     bool isMonthCached(QString station, QDate month);
 
-    bool rangeRequest;
-    bool dataSetQueuePrep;
+
 
     // for live data stuff
     QScopedPointer<QNetworkAccessManager> liveNetAccessManager;
