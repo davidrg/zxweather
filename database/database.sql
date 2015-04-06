fad9010ed321f41f533802641a4ff96ee685f9b7
David Goodwin <david@zx.net.nz>
2015-04-06 17:42:53 +1200

    Add index on replication status table

--- database/database.sql
+++ database/database.sql
@@ -228,6 +228,11 @@ comment on column replication_status.status is 'Status of the sample on the remo
 comment on column replication_status.status_time is 'Time the status last changed';
 comment on column replication_status.retries is 'Number of times the sample has been transmitted';
 
+CREATE INDEX replication_status_status_site_id_idx
+  ON replication_status
+  USING btree
+  (status, site_id);
+
 create table davis_forecast_rule(
   rule_id integer not null primary key,
   description character varying
