fd5a227602559e2d1c58bd6d877d57591ae9e98b
David Goodwin <david@zx.net.nz>
2018-05-29 22:15:37 +1200

    Add postgres version of storm rain, minor hanges to template and sqlite version

--- desktop/reporting/reports/storm_rain/postgres.sql
+++ desktop/reporting/reports/storm_rain/postgres.sql
@@ -1,6 +1,8 @@
 -- This needs the following index for anything approaching reasonable performance:
 -- create index idx_rain_time_station on sample(station_id, time_stamp asc, rainfall asc);
 
+--drop index idx_rain_time_station;
+
 -- This query should find all storms along with their total rainfall.
 -- Storms with less than 0.4mm of rain are ignored as in all cases these
 -- are a single click of the rain gauge with 24 hours of no rain to either
@@ -13,7 +15,7 @@ with marked_storm_rows as (
     -- A storm ends when:
     case when nc.next is null
       then 1 -- There is no newer data (storm in progress effectively)
-    when nc.next - s.time_stamp > 86400
+    when nc.next - s.time_stamp > '24 hours':: interval
       then 1 -- Next row with rain is more than 24h in the future
     else 0 end                                       as end_of_storm,
     -- otherwise this row is not the end of a storm
@@ -21,7 +23,7 @@ with marked_storm_rows as (
     -- A storm potentially starts when
     case when cp.prev is null
       then 1 -- There is no older data (storm in progress effectively)
-    when s.time_stamp - cp.prev > 86400
+    when s.time_stamp - cp.prev > '24 hours'::interval
       then 1 -- Next next oldest row with rain is more than 24h ago
     else 0 end                                       as start_of_storm -- otherwise this row can not be the start of a storm
   -- For a storm to actually start we need 0.4m of rain or greater.
@@ -63,9 +65,9 @@ with marked_storm_rows as (
                             and cur.rainfall > 0
                             and prev.rainfall > 0
                     ) cp on cp.station_id = s.station_id and cp.cur = s.time_stamp
-  inner join station stn on stn.station_id = s.station_id
-  where stn.code = :stationCode
-    and s.rainfall > 0
+    inner join station stn on stn.station_id = s.station_id
+  where s.rainfall > 0
+    and stn.code = :stationCode
 ), storm_terminators as (
   select
     this_time_stamp as time_stamp,
@@ -99,20 +101,14 @@ with marked_storm_rows as (
 )
 
 select
-  datetime(storm.storm_start_time, 'unixepoch', 'localtime') as start_time,
-  datetime(storm.storm_end_time, 'unixepoch', 'localtime') as end_time,
-  case
-  when ((storm.storm_end_time - storm.storm_start_time) / 86400) > 0
-    then ((storm.storm_end_time - storm.storm_start_time) / 86400) || ' days ' || strftime('%H:%M:%S', (storm.storm_end_time - storm.storm_start_time) % 86400, 'unixepoch')
-    else strftime('%H:%M:%S', (storm.storm_end_time - storm.storm_start_time) % 86400, 'unixepoch')
-  end as duration,
-  cast(round(sum(s.rainfall),1) as text) as total_rainfall
+  storm.storm_start_time as start_time,
+  storm.storm_end_time as end_time,
+  storm.storm_end_time - storm.storm_start_time as duration,
+  cast(round(sum(s.rainfall::numeric),1) as text) as total_rainfall
 from storms storm
 inner join sample s on s.time_stamp between storm.storm_start_time and storm.storm_end_time
   and s.station_id = storm.station_id
-inner join station stn on stn.station_id = storm.station_id
-  where stn.code = :stationCode
-  and storm.storm_start_time between :start_t and :end_t
+  where storm.storm_start_time between :start and :end
 group by storm.storm_start_time, storm.storm_end_time, storm.station_id
-having sum(s.rainfall) > 0.2 -- Filter out storms smaller than 0.4mm - isolated rain clicks don't count
+having sum(s.rainfall) >= 0.4 -- Filter out storms smaller than 0.4mm - isolated rain clicks don't count
 order by sum(s.rainfall) desc;
