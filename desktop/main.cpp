4174e9f2d5ed218fa06f284d750efb4faeb60e7c
David Goodwin <david@zx.net.nz>
2019-06-05 20:35:16 +1200

    Write log messages to desktop.log if that file exists and is writable.

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -21,10 +21,75 @@
  ****************************************************************************/
 
 #include <QApplication>
+#include <QtDebug>
+#include <QFile>
+#include <QTextStream>
 #include "mainwindow.h"
 
-int main(int argc, char *argv[])
+#define LOG_FILE "desktop.log"
+
+#ifndef qInfo
+#define qInfo qDebug
+#endif
+
+static QFile _global_log_file;
+static QTextStream _log_stream;
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5,0,0))
+void msgFileHandler(QtMsgType type, const QMessageLogContext &, const QString & msg)
+#else
+ void msgFileHandler(QtMsgType type, const char *msg)
+#endif
 {
+    QString output_line;
+    switch (type) {
+    case QtDebugMsg:
+        output_line = QString("Debug: %1").arg(msg);
+        break;
+#if (QT_VERSION >= QT_VERSION_CHECK(5,0,0))
+    case QtInfoMsg:
+        output_line = QString("Info: %1").arg(msg);
+        break;
+#endif
+    case QtWarningMsg:
+        output_line = QString("Warning: %1").arg(msg);
+    break;
+    case QtCriticalMsg:
+        output_line = QString("Critical: %1").arg(msg);
+    break;
+    case QtFatalMsg:
+        output_line = QString("Fatal: %1").arg(msg);
+    break;
+    }
+    _log_stream << output_line << endl;
+}
+
+int main(int argc, char *argv[])
+{    
+    _global_log_file.setFileName(LOG_FILE);
+    if (_global_log_file.exists() && _global_log_file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        qInfo() << "Log file" << LOG_FILE << "found and opened successfully! Redirecting log output...";
+
+        _log_stream.setDevice(&_global_log_file);
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5,0,0))
+        qInstallMessageHandler(msgFileHandler);
+#else
+        qInstallMsgHandler(msgFileHandler);
+#endif
+    } else if (!_global_log_file.exists()) {
+#ifdef Q_OS_WIN
+        fprintf(stdout, "Log file " LOG_FILE " not found. Log output will be sent to the debugger.");
+#endif
+        qInfo() << "Log file" << LOG_FILE << "not found.";
+
+    } else {
+#ifdef Q_OS_WIN
+        fprintf(stderr, "Failed to open log file " LOG_FILE " for write+append. Log output will be sent to the debugger.");
+#endif
+        qWarning() << "Failed to open log file" << LOG_FILE << "for write+append.";
+    }
+
     QApplication a(argc, argv);
 
     QCoreApplication::setOrganizationName("zxnet");
