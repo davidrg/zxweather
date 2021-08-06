1a727ce456a9031493dee302bcfe1d5c9a98f6cb
David Goodwin <david@zx.net.nz>
2021-08-06 23:27:20 +1200

    Desktop: Add marked gaps report
    This will show any gaps marked as permanent when connected via the Web Interface.

---
+++ desktop/reporting/reports/marked_gaps/station_info_sqlite.sql
@@ -0,0 +1,7 @@
+select stn.title,
+       stn.archived,
+       datetime(stn.archived_time, 'unixepoch', 'localtime') as archived_time,
+       stn.archived_message,
+       case when exists (select 1 from sample_gap where station_id = stn.station_id) then true else false end as has_known_gaps
+from station stn
+where lower(stn.code) = lower(:stationCode)
